9db4663e9485c1d3c2bf000f35efee043f67a998
Alex <awei799331@gmail.com>
2019-05-28 18:01:59 -0400

    converted more images

--- convertImage.py
+++ convertImage.py
@@ -1,14 +1,16 @@
 from PIL import Image
 import numpy
+import os
 
+size = (128,128)
 #https://stackoverflow.com/questions/18777873/convert-rgb-to-black-or-white/18778280
 
-colorImage = Image.open("orion.jpg")
-grayImage = colorImage.convert('L')
-blackWhiteImage = grayImage.point(lambda x: 0 if x<128 else 255, '1')
-blackWhiteImage.save("orion-bw.jpg")
-size = (128,128)
-blackWhiteImage = blackWhiteImage.resize(size, 0)
-blackWhiteImage.save("orion-bw-resized.jpg")
+for file_name in os.listdir("new-images/Ursa_Major/"):
+    colorImage = Image.open("new-images/Ursa_Major/" + file_name)
+    grayImage = colorImage.convert('L')
+    blackWhiteImage = grayImage.point(lambda x: 0 if x<128 else 255, '1')
+    blackWhiteImage = blackWhiteImage.resize(size, 0)
+    blackWhiteImage.save("new-images/Ursa_Major/" + file_name[0:-4] + "-resized.jpg")
+
 
 arr = numpy.ndarray.tolist(numpy.array(blackWhiteImage.getdata(),numpy.uint8).reshape(blackWhiteImage.size[1], blackWhiteImage.size[0]))
