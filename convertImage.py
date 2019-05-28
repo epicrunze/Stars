530f9aeb6afd0113e95936000561ba1c0250b713
Alex <awei799331@gmail.com>
2019-05-28 17:15:57 -0400

    can resize image

--- convertImage.py
+++ convertImage.py
@@ -7,5 +7,8 @@ colorImage = Image.open("orion.jpg")
 grayImage = colorImage.convert('L')
 blackWhiteImage = grayImage.point(lambda x: 0 if x<128 else 255, '1')
 blackWhiteImage.save("orion-bw.jpg")
+size = 128,128
+blackWhiteImage = blackWhiteImage.resize(size, 0)
+blackWhiteImage.save("orion-bw-resized.jpg")
 
 arr = numpy.ndarray.tolist(numpy.array(blackWhiteImage.getdata(),numpy.uint8).reshape(blackWhiteImage.size[1], blackWhiteImage.size[0]))
